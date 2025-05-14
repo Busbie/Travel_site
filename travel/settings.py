7ecec4dd7c311225ece1816b4fe1c06c9a77e154
Busbie <ojediranbusayo0112@gmail.com>
2025-05-14 10:26:16 +0100

    add static files

--- travel/settings.py
+++ travel/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['your-service-name.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
